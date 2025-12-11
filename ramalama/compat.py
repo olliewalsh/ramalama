dd9c1feef919ed8443f59d5a91ff6c3076eb0834
Oliver Walsh <owalsh@redhat.com>
2025-12-11 11:48:40 +0000

    Workaround missing StrEnum in python 3.10
    Signed-off-by: Oliver Walsh <owalsh@redhat.com>

---
+++ ramalama/compat.py
@@ -0,0 +1,12 @@
+"""ramalama compat module."""
+
+# Python 3.10 doesn't have StrEnum
+try:
+    from enum import StrEnum
+except ImportError:
+    from enum import Enum
+
+    class StrEnum(str, Enum):  # type: ignore[no-redef]
+        """StrEnum class for Python 3.10."""
+
+        pass
